9cc9dc4ce4f96c8b7a30874fa7ebec1dee9a5950
Himangshu1086 <baishyahimangshu499@gmail.com>
2023-11-07 20:21:54 +0530

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 require("./db/intiDb");
 
