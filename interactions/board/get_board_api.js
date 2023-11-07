12d678962bc6381888ff9739c224e069c3b45247
Himangshu1086 <baishyahimangshu499@gmail.com>
2023-11-07 20:46:52 +0530

    update

--- interactions/board/get_board_api.js
+++ interactions/board/get_board_api.js
@@ -5,7 +5,8 @@ const Board = require("../../db/models/boardSchema");
 const getBoard = async (req, res) => {
     try{
       const boards = await Board.find();
-      res.status(200).json({boards});
+      // res.status(200).json({boards});
+      res.send(boards)
     }
     catch{
       err=>{
